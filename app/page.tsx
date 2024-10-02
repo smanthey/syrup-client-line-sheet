e39700dfb2772a28510dc43f6add4e0c4450eccc
smanthey <scottman@me.com>
2024-10-02 00:07:27 -0700

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ClientLineSheet from '../components/client-line-sheet'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen p-24">
-      <ClientLineSheet />
-    </main>
-  )
-}
